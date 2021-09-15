de0624bef2ec617a1eec10ac300a4188da9f79ce
Maja Kersten <74767271+majak27@users.noreply.github.com>
2021-09-15 14:52:24 +0000

    meerdere cirkels (mbv classes)

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-var x, y, v;
+let drawBall;
+let circles = [];
 
 function setup() {
   createCanvas(500,500);
-
-  x = 300
-  y = 50
-  v = 3
-  u = 3
+  for(let i = 0; i < 25; i++) {
+    circles[i] = new Circle(random(width), random(height), random(-3, 3), random(-3, 3));
+  }
 }
 
 function draw() {
-  background(225);
-
-  rect(x,y,50,50);
-  x = x + v;
-  y = y + u;
-  
-  if (x <= 0 || x > 450){
-    v = v * -1;
+  background(230);
+  for(let i = 0; i < circles.length; i++) {
+    circles[i].move();
+    circles[i].display();
+    }
   }
-  if (y <= 0 || y > 450){
-    u = u * -1;
+
+class Circle {
+  constructor(x, y, v, u, color, size, radius) {
+    this.x = x;
+    this.y = y;
+    this.v = v;
+    this.u = u;
+    this.color = (random(0,255),random(0,255),random(0,255),random(0,255))
+    this.size = random(10,100)
+    this.radius = (this.size/2)
+  }
+
+  move() {
+    this.x += this.v;
+    if (this.x < 25 || this.x > width) {
+      this.v *= -1;
+    }
+    this.y += this.u;
+    if (this.y < 25 || this.y > height) {
+      this.u *= -1;
+    }
+    if (this.x > 150|| this.x == 350) {
+      this.x *= -1
+    }
+  }
+
+  display() {
+    ellipse(this.x, this.y, this.size);
+    fill(this.color);
+    noStroke();
   }
 }
